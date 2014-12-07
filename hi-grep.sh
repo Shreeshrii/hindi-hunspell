9d29bde7343bb6d0ac85da8ba82b3b1049fae095
Shree Devi Kumar <shreeshrii@gmail.com>
2014-12-07 18:12:37 +0530

    update verbs dic

--- hi-grep.sh
+++ hi-grep.sh
@@ -1,5 +1,4 @@
-LC_ALL=hi_IN.utf8 grep ा$ hi-nane.txt >hi-ne.txt
-LC_ALL=hi_IN.utf8 sort hi-nane.txt | uniq >tmp1.txt
-LC_ALL=hi_IN.utf8 sort hi-ne.txt | uniq > tmp2.txt
-LC_ALL=hi_IN.utf8 comm -23 tmp1.txt tmp2.txt >hi-na.txt
+LC_ALL=hi_IN.utf8 sort hi-verbs-addl1.txt | uniq >tmp1.txt
+LC_ALL=hi_IN.utf8 sort hi-verb2.dic | uniq > tmp2.txt
+LC_ALL=hi_IN.utf8 comm -23 tmp1.txt tmp2.txt >hi-verb2.txt
 
