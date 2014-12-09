5dc6245353e2822513954df1ed8882ffb59ad89f
Shree Devi Kumar <shreeshrii@gmail.com>
2014-12-09 20:12:06 +0530

    Add noun .dic and .aff files

--- hi-misc.sh
+++ hi-misc.sh
@@ -1,3 +1,2 @@
-cat hi-misc0.dic hi-misc1.dic hi-misc2.dic >tmp1.txt
-LC_ALL=hi_IN.utf8  sed -f sed-normalize.txt tmp1.txt >hi-misc.dic
+cat hi-misc0.dic hi-misc1.dic hi-misc2.dic hi-misc3.dic hi-misc9.dic>hi-misc.dic
 LC_ALL=hi_IN.utf8  ./unmunch.sh hi-misc.dic hi-misc.aff | LC_ALL=hi_IN.utf8 sort | uniq >hi-misc.txt
---
+++ hi-nouns.sh
@@ -0,0 +1,2 @@
+cat hi-noun2.dic hi-noun3.dic hi-noun3a.dic  hi-noun4.dic hi-noun4a.dic hi-noun5.dic hi-noun6.dic  hi-noun7.dic hi-noun8.dic hi-noun8a.dic hi-noun9.dic>hi-nouns.dic
+LC_ALL=hi_IN.utf8  ./unmunch.sh hi-nouns.dic hi-nouns.aff | LC_ALL=hi_IN.utf8 sort | uniq >hi-nouns.txt
