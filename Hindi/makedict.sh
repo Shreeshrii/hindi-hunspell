9bcf84007a904b332f8ad1fe96cbaf357c940555
Shree Devi Kumar <shreeshrii@gmail.com>
2017-03-13 18:03:37 +0530

    revert and fix

--- Hindi/makedict.sh
+++ Hindi/makedict.sh
@@ -9,12 +9,7 @@ hi-num.dic  blank.txt \
 hi-prefix.dic blank.txt \
 hi-verbs.dic  blank.txt \
 hi-nouns.dic  blank.txt \
- > tmp 
-sed -e 's/^[0-9]//g' tmp   > tmp1
-sed -e 's/^[0-9]//g' tmp1 > tmp2
-sed -e 's/^[0-9]//g' tmp2 > tmp3
-sed -e 's/^[0-9]//g' tmp3 > tmp4
-sed -e '/^$/d' tmp4>tmp5
+ > tmp5 
 LC_ALL=hi_IN.utf8   sort tmp5 > tmp6
 LC_ALL=hi_IN.utf8   uniq tmp6 >tmp7
 cat head.dic tmp7 > hi_IN.dic
@@ -22,3 +17,5 @@ cat head.dic tmp7 > hi_IN.dic
 rm tmp*
 cp hi_IN.dic /mnt/c/Users/User/AppData/Roaming/Notepad++/plugins/config/Hunspell
 cp hi_IN.aff /mnt/c/Users/User/AppData/Roaming/Notepad++/plugins/config/Hunspell
+cp hi_IN.dic ../dict-hi_IN
+cp hi_IN.aff  ../dict-hi_IN
