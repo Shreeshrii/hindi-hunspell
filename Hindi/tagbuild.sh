94ef33d463b18cbd25b26b99c2f70aac4fcb72ae
Shree Devi Kumar <shreeshrii@gmail.com>
2017-03-22 16:56:24 +0530

    Version 2.1.2

--- Hindi/tagbuild.sh
+++ Hindi/tagbuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git tag
-git tag -a v2.1.1 -m "Version 2.1.1"
+git tag -a v2.1.2 -m "Version 2.1.2"
 cd ..
 git add --all
-git commit -m 'Version 2.1.1'
+git commit -m 'Version 2.1.2'
 git push origin master
 git push origin --tags
