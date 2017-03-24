047c57a51c59ce78c47a5a992c77e307d41efd2d
Shree Devi Kumar <shreeshrii@gmail.com>
2017-03-24 18:34:34 +0530

    update files

--- tools/aspell-munch-etc.sh
+++ tools/aspell-munch-etc.sh
@@ -13,4 +13,15 @@ LC_ALL=hi_IN.utf8   aspell munch-list keep   -l hi < hi_IN.txt > hi_IN.dic
  
  echo "Sort, Unique and then Add 500000 to top of .dic file"
  
+  aspell -l hi expand 3 <hi_IN.dic >hi_IN-expand.txt
+
+  cut -d ' ' -f2 hi_IN-expand.txt > hi-In-2.txt
+
+   sed -i -f ../tools/nukta-combine.sed hi-In-2.txt
+   
+   LC_ALL=hi_IN.utf8   aspell munch-list keep   -l hi < hi-In-2.txt > hi_IN.dic
+       
+ echo "Sort, Unique and then Add 500000 to top of .dic file"
+   
+    sed -i -f ../tools/nukta-combine.sed hi_IN.dic
  
